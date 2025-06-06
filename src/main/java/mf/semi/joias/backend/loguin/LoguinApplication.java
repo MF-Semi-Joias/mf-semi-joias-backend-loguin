376f3274ee56946be30242c2b682fa9d2d8804f9
Flavio <samus.sp@gmail.com>
2025-06-05 22:58:04 -0300

    commit inicial

---
+++ src/main/java/mf/semi/joias/backend/loguin/LoguinApplication.java
@@ -0,0 +1,13 @@
+package mf.semi.joias.backend.loguin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoguinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoguinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mf/semi/joias/backend/loguin/LoguinApplicationTests.java
@@ -0,0 +1,13 @@
+package mf.semi.joias.backend.loguin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoguinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
